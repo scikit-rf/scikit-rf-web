c42cc39856b53e187ddd59094e2cf67462cec43a
alex arsenovic <arsenovic@virginia.edu>
2013-01-21 08:33:43 -0500

    :q!

--- conf.py
+++ conf.py
@@ -125,7 +125,7 @@ html_short_title = "scikit-rf"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "scikit-rf-title-flat.png"
+html_logo = "scikit-rf-title-flat.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # pixels large.
